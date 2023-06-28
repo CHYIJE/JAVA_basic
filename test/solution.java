33767186ed28958f6a34e9801b938a8b17d8a464
CYJ <you@example.com>
2023-06-28 17:32:51 +0900

    기본시작

---
+++ test/solution.java
@@ -0,0 +1,14 @@
+package test;
+
+public class solution {
+        public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+
+public static void main(String[] args) {
+    System.out.println(new solution().solution(2, 3)); 
+}
+    
+}
+
