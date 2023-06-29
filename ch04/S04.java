18f6bc624c0bbb14839bba7c4ce2b2a6dd8e61a5
CYJ <you@example.com>
2023-06-29 16:57:55 +0900

    로또

---
+++ ch04/S04.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class S04 {
+    public static void main(String[] args) {
+    
+        double num = Math.random() * 45;
+        int intNum = (int) num;
+        int result = intNum +1;
+        
+        System.out.println(result);
+
+        
+
+    }
+}
