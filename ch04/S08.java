4c58b5d37201001aba5dc450c352be4046a60411
CYJ <you@example.com>
2023-06-30 09:53:08 +0900

    continue

--- ch04/S08.java
+++ ch04/S08.java
@@ -4,16 +4,17 @@ public class S08 {
     public static void main(String[] args) {
         //continue
 
-        int i = 1;
+        int i = 0;
 
         while(i < 1000){
-            if(i % 2 == 0){
+            i++;
+            if(i % 2 != 0){
                 // continue를 사용한 위치에서 현재 반복회차를 중지한다
                 // 다음 반복으로 넘어간다
                 continue;
             }
             System.out.println(i); 
-            i++;
+            
 
         }
     }
