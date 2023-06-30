615dbd8108452923d2a6a4b946c4da2cccf01415
CYJ <you@example.com>
2023-06-30 10:58:44 +0900

    do-while

---
+++ ch04/S11.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class S11 {
+    public static void main(String[] args) {
+        // do while 무저건 1번은 작동되어야 한다.
+
+        int i =1;
+
+        do{
+            System.out.println(i);
+            i++;
+        }while(i < 11);
+
+    }
+}
