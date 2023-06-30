779f7b5d2f270adc3660ff841484acd3714c1d02
CYJ <you@example.com>
2023-06-30 10:00:12 +0900

    continue

--- ch04/S08.java
+++ ch04/S08.java
@@ -16,7 +16,7 @@ public class S08 {
 
         while(i < 1000){
             i++;
-            if(i % 2 != 0){     //이 조건에서느 sysout 작동 안하겠다
+            if(i % 2 != 0){     //이 조건에서는 sysout 작동 안하겠다
                 // continue를 사용한 위치에서 현재 반복회차를 중지한다
                 // 다음 반복으로 넘어간다
                 continue; // 배치 위치를 잘 지정해야한다
