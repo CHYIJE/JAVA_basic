f7b715cd1bd2c3469e23a1e0982a88c47e6dd1a1
CYJ <you@example.com>
2023-06-30 09:54:05 +0900

    continue

--- ch04/S08.java
+++ ch04/S08.java
@@ -11,7 +11,7 @@ public class S08 {
             if(i % 2 != 0){
                 // continue를 사용한 위치에서 현재 반복회차를 중지한다
                 // 다음 반복으로 넘어간다
-                continue;
+                continue; // 배치 위치를 잘 지정해야한다
             }
             System.out.println(i); 
             
