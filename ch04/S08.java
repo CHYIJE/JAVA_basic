2f110c8176720b30e918c41db87c0a04da107375
CYJ <you@example.com>
2023-06-30 09:50:29 +0900

    continue

--- ch04/S08.java
+++ ch04/S08.java
@@ -1,7 +1,20 @@
 package ch04;
-
+//ctrl + shift + 위아래 (해당 줄 옮기기)
 public class S08 {
     public static void main(String[] args) {
-        
+        //continue
+
+        int i = 1;
+
+        while(i < 1000){
+            if(i % 2 == 0){
+                // continue를 사용한 위치에서 현재 반복회차를 중지한다
+                // 다음 반복으로 넘어간다
+                continue;
+            }
+            System.out.println(i); 
+            i++;
+
+        }
     }
 }
