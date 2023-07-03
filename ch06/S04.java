c3369959013119420e08586f7f8f766949d5c2ba
ysh <dbtmdgks7897@outlook.kr>
2023-07-03 15:52:45 +0900

    객체지향

--- ch06/S04.java
+++ ch06/S04.java
@@ -1,33 +1,33 @@
-package ch06;
+// package ch06;
 
-class Slave {
-    static int countSlave = 0;
-    String name;
+// class Slave {
+//     static int countSlave = 0;
+//     String name;
 
-    public Slave() {
-        this.name = "노예들";
-        Slave.addSlave();
-    }
+//     public Slave() {
+//         this.name = "노예들";
+//         Slave.addSlave();
+//     }
 
-    public static void addSlave(){
-        Slave.countSlave++;
-        System.out.println("새로운 노예가 잡혀왔습니다");
-    }
+//     public static void addSlave(){
+//         Slave.countSlave++;
+//         System.out.println("새로운 노예가 잡혀왔습니다");
+//     }
 
    
     
-}
+// }
 
-public class S04 {
-    public static void main(String[] args) {
+// public class S04 {
+//     public static void main(String[] args) {
 
-        new Slave();
-        new Slave();
-        new Slave();
-        new Slave();
-        new Slave();
+//         new Slave();
+//         new Slave();
+//         new Slave();
+//         new Slave();
+//         new Slave();
         
-        System.out.println("축하드립니다! 노예의 수는 " + Slave.countSlave  + "명 입니다.");
+//         System.out.println("축하드립니다! 노예의 수는 " + Slave.countSlave  + "명 입니다.");
         
-    }
-}
+//     }
+// }
