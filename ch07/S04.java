d97752d37c4eaa69194d861d040941bda292aca4
ysh <dbtmdgks7897@outlook.kr>
2023-07-04 16:10:44 +0900

    롤챔

--- ch07/S04.java
+++ ch07/S04.java
@@ -1,239 +1,239 @@
-package ch07;
+// package ch07;
 
-// 모든 스킬들의 공통적인 부분을 세팅한다
-// MAX_POINT 모든 스킬들이 공통적으로 사용하는 변하지 않는 값을 세팅(static final)
-// 스킬마다 바뀔 수 있는 부분 name, point, champ는 필드(멤버변수)로 작성한다
-class Skill {
-    static final int MAX_POINT = 5;
-    String name;
-    int point;
-    Champ champ;
+// // 모든 스킬들의 공통적인 부분을 세팅한다
+// // MAX_POINT 모든 스킬들이 공통적으로 사용하는 변하지 않는 값을 세팅(static final)
+// // 스킬마다 바뀔 수 있는 부분 name, point, champ는 필드(멤버변수)로 작성한다
+// class Skill {
+//     static final int MAX_POINT = 5;
+//     String name;
+//     int point;
+//     Champ champ;
 
-    public Skill(String name, Champ champ) {
-        this.name = name;
-        this.champ = champ;
-        this.point = 0;
-    }
+//     public Skill(String name, Champ champ) {
+//         this.name = name;
+//         this.champ = champ;
+//         this.point = 0;
+//     }
 
-    public int getDamage() {
-        return 0;
-    }
+//     public int getDamage() {
+//         return 0;
+//     }
 
-    public void plusPoint() {
-        if(this.point >= MAX_POINT){
-            this.point = MAX_POINT;
-            System.out.println("포인트를 더 이상 올릴 수 없습니다.");
-            return;
-        }
-        this.point++;
-    }
-}
+//     public void plusPoint() {
+//         if(this.point >= MAX_POINT){
+//             this.point = MAX_POINT;
+//             System.out.println("포인트를 더 이상 올릴 수 없습니다.");
+//             return;
+//         }
+//         this.point++;
+//     }
+// }
 
-class Volly extends Skill {
-    public Volly(Champ champ){
-        super("일제사격", champ);
-    }
+// class Volly extends Skill {
+//     public Volly(Champ champ){
+//         super("일제사격", champ);
+//     }
 
-    // 부모가 가진 메소드 형태를 그대로 가져온 상태에서
-    // 내요만 바꿔서 세팅한다.
-    @Override
-    public int getDamage() {
-        // 고정 데미지 20 / 35 / 50 / 65 / 80
-        // + 캐릭터 공격력 * 1.0
+//     // 부모가 가진 메소드 형태를 그대로 가져온 상태에서
+//     // 내요만 바꿔서 세팅한다.
+//     @Override
+//     public int getDamage() {
+//         // 고정 데미지 20 / 35 / 50 / 65 / 80
+//         // + 캐릭터 공격력 * 1.0
 
-        // 스킬을 찍지 않았을 때
-        if(point < 1) {
-            System.out.println("스킬을 사용할 수 없습니다.");
-            return super.getDamage();
-        }
+//         // 스킬을 찍지 않았을 때
+//         if(point < 1) {
+//             System.out.println("스킬을 사용할 수 없습니다.");
+//             return super.getDamage();
+//         }
 
-        int damage = (int)(20 + (15 * (point - 1)) + (this.champ.ad * 1.0));
+//         int damage = (int)(20 + (15 * (point - 1)) + (this.champ.ad * 1.0));
 
-        return damage;
-    }
+//         return damage;
+//     }
 
-}
+// }
 
 
 
-// 챔피언의 공통적인 부분들 만들어 준다
-class Champ {
-    // 물리 공격력
-    int ad;
-    Skill skill;
+// // 챔피언의 공통적인 부분들 만들어 준다
+// class Champ {
+//     // 물리 공격력
+//     int ad;
+//     Skill skill;
 
-    public Champ(int ad, Skill skill) {
-        this.ad = ad;
-        this.skill = skill;
-    }
+//     public Champ(int ad, Skill skill) {
+//         this.ad = ad;
+//         this.skill = skill;
+//     }
 
     
-    // public Champ(int ad) {
-    //     this.ad = ad;
-    // }
+//     // public Champ(int ad) {
+//     //     this.ad = ad;
+//     // }
     
-}
+// }
 
-// 챔피언의 공통적인 부분을 이어 받되
-// 개별 캐릭터의 특징을 다시 세팅 해준다
-class 애쉬 extends Champ {
-    public 애쉬() {
-        // 1. 상속받는 객체가 생성됨
-        // 2. super를 통해서 객체 안의 필드에 값을 초기화해줌
-        super(100, null);
-        // 3. 현재 class의 객체가 생성됨
-        // 4. 현재 객체 안의 필드에 값을 넣어줌
-        this.skill = new Volly(this);
-    }
-}
-
-
-class Peace extends Skill {
-    public Peace(Champ champ){
-        super("필드오버 피스메이커", champ);
-    }
-
-    // 부모가 가진 메소드 형태를 그대로 가져온 상태에서
-    // 내요만 바꿔서 세팅한다.
-    @Override
-    public int getDamage() {
-     // 스킬 이름 : 필드오버 피스 메이커
-    // 고정 공격력 : 50 90 130 170 210
-    // + 캐릭터 공격력* 1.25/1.45/1.55/1.65/1.85/ 2.05
-
-        // 스킬을 찍지 않았을 때
-        if(point < 1) {
-            System.out.println("스킬을 사용할 수 없습니다.");
-            return super.getDamage();
-        }
-
-        int damage = (int)(50 + (40 * (point - 1)) + (this.champ.ad * (1.25 + (0.2 * (point -1)))));
-
-        return damage;
-    }
-
-}
-
-class 케이틀린 extends Champ {
-    public 케이틀린() {
-        // 1. 상속받는 객체가 생성됨
-        // 2. super를 통해서 객체 안의 필드에 값을 초기화해줌
-        super(96, null);
-        // 3. 현재 class의 객체가 생성됨
-        // 4. 현재 객체 안의 필드에 값을 넣어줌
-        this.skill = new Peace(this);
-    }
-}
-
-class Tempest extends Skill {
-    public Tempest(Champ champ){
-        super("강철폭풍", champ);
-    }
-
-    // 부모가 가진 메소드 형태를 그대로 가져온 상태에서
-    // 내요만 바꿔서 세팅한다.
-    @Override
-    public int getDamage() {
-        // 고정 데미지 20 / 35 / 50 / 65 / 80
-        // + 캐릭터 공격력 * 1.0
-
-        // 스킬을 찍지 않았을 때
-        if(point < 1) {
-            System.out.println("스킬을 사용할 수 없습니다.");
-            return super.getDamage();
-        }
-
-        int damage = (int)(20 + (15 * (point - 1)) + (this.champ.ad * 1.05));
-
-        return damage;
-    }
-
-}
-
-class 야스오 extends Champ {
-    public 야스오() {
-        // 1. 상속받는 객체가 생성됨
-        // 2. super를 통해서 객체 안의 필드에 값을 초기화해줌
-        super(100, null);
-        // 3. 현재 class의 객체가 생성됨
-        // 4. 현재 객체 안의 필드에 값을 넣어줌
-        this.skill = new Tempest(this);
-    }
-}
+// // 챔피언의 공통적인 부분을 이어 받되
+// // 개별 캐릭터의 특징을 다시 세팅 해준다
+// class 애쉬 extends Champ {
+//     public 애쉬() {
+//         // 1. 상속받는 객체가 생성됨
+//         // 2. super를 통해서 객체 안의 필드에 값을 초기화해줌
+//         super(100, null);
+//         // 3. 현재 class의 객체가 생성됨
+//         // 4. 현재 객체 안의 필드에 값을 넣어줌
+//         this.skill = new Volly(this);
+//     }
+// }
 
 
-public class S04 {
-    public static void main(String[] args) {
+// class Peace extends Skill {
+//     public Peace(Champ champ){
+//         super("필드오버 피스메이커", champ);
+//     }
 
-        // // 참조형 변수에 주소값을 아직 모르는 상태에서
-        // // 초기화를 해두려고 하는 경우 null을 사용한다
-        // String str = null;
-        // System.out.println(str);
-        // str = new String("홍길동");
-        // System.out.println(str);
-        // // 더 이상 쓸모 없는 객체에 null을 입력한다.
-        // str = null;
+//     // 부모가 가진 메소드 형태를 그대로 가져온 상태에서
+//     // 내요만 바꿔서 세팅한다.
+//     @Override
+//     public int getDamage() {
+//      // 스킬 이름 : 필드오버 피스 메이커
+//     // 고정 공격력 : 50 90 130 170 210
+//     // + 캐릭터 공격력* 1.25/1.45/1.55/1.65/1.85/ 2.05
 
-        // 애쉬 champ1 = new 애쉬();
+//         // 스킬을 찍지 않았을 때
+//         if(point < 1) {
+//             System.out.println("스킬을 사용할 수 없습니다.");
+//             return super.getDamage();
+//         }
 
-        // System.out.println(champ1.skill.getDamage());
+//         int damage = (int)(50 + (40 * (point - 1)) + (this.champ.ad * (1.25 + (0.2 * (point -1)))));
 
-        // champ1.skill.plusPoint();
+//         return damage;
+//     }
 
-        // System.out.println(champ1.skill.getDamage());
+// }
 
-        // champ1.skill.plusPoint();
+// class 케이틀린 extends Champ {
+//     public 케이틀린() {
+//         // 1. 상속받는 객체가 생성됨
+//         // 2. super를 통해서 객체 안의 필드에 값을 초기화해줌
+//         super(96, null);
+//         // 3. 현재 class의 객체가 생성됨
+//         // 4. 현재 객체 안의 필드에 값을 넣어줌
+//         this.skill = new Peace(this);
+//     }
+// }
 
-        // System.out.println(champ1.skill.getDamage());
+// class Tempest extends Skill {
+//     public Tempest(Champ champ){
+//         super("강철폭풍", champ);
+//     }
+
+//     // 부모가 가진 메소드 형태를 그대로 가져온 상태에서
+//     // 내요만 바꿔서 세팅한다.
+//     @Override
+//     public int getDamage() {
+//         // 고정 데미지 20 / 35 / 50 / 65 / 80
+//         // + 캐릭터 공격력 * 1.0
+
+//         // 스킬을 찍지 않았을 때
+//         if(point < 1) {
+//             System.out.println("스킬을 사용할 수 없습니다.");
+//             return super.getDamage();
+//         }
+
+//         int damage = (int)(20 + (15 * (point - 1)) + (this.champ.ad * 1.05));
+
+//         return damage;
+//     }
+
+// }
+
+// class 야스오 extends Champ {
+//     public 야스오() {
+//         // 1. 상속받는 객체가 생성됨
+//         // 2. super를 통해서 객체 안의 필드에 값을 초기화해줌
+//         super(100, null);
+//         // 3. 현재 class의 객체가 생성됨
+//         // 4. 현재 객체 안의 필드에 값을 넣어줌
+//         this.skill = new Tempest(this);
+//     }
+// }
 
 
+// public class S04 {
+//     public static void main(String[] args) {
 
-        // 케이틀린 champ2 = new 케이틀린();
+//         // // 참조형 변수에 주소값을 아직 모르는 상태에서
+//         // // 초기화를 해두려고 하는 경우 null을 사용한다
+//         // String str = null;
+//         // System.out.println(str);
+//         // str = new String("홍길동");
+//         // System.out.println(str);
+//         // // 더 이상 쓸모 없는 객체에 null을 입력한다.
+//         // str = null;
 
-        // System.out.println(champ2.skill.getDamage());
+//         // 애쉬 champ1 = new 애쉬();
 
-        // champ2.skill.plusPoint();
+//         // System.out.println(champ1.skill.getDamage());
 
-        // System.out.println(champ2.skill.getDamage());
+//         // champ1.skill.plusPoint();
 
-        // champ2.skill.plusPoint();
+//         // System.out.println(champ1.skill.getDamage());
 
-        // System.out.println(champ2.skill.getDamage());
+//         // champ1.skill.plusPoint();
+
+//         // System.out.println(champ1.skill.getDamage());
 
 
 
-        // 야스오 champ3 = new 야스오();
+//         // 케이틀린 champ2 = new 케이틀린();
 
-        // System.out.println(champ3.skill.getDamage());
+//         // System.out.println(champ2.skill.getDamage());
 
-        // champ3.skill.plusPoint();
+//         // champ2.skill.plusPoint();
 
-        // System.out.println(champ3.skill.getDamage());
+//         // System.out.println(champ2.skill.getDamage());
 
-        // champ3.skill.plusPoint();
+//         // champ2.skill.plusPoint();
 
-        // System.out.println(champ3.skill.getDamage());
+//         // System.out.println(champ2.skill.getDamage());
+
+
+
+//         // 야스오 champ3 = new 야스오();
+
+//         // System.out.println(champ3.skill.getDamage());
+
+//         // champ3.skill.plusPoint();
+
+//         // System.out.println(champ3.skill.getDamage());
+
+//         // champ3.skill.plusPoint();
+
+//         // System.out.println(champ3.skill.getDamage());
 
         
 
-Champ[] champArray = new Champ[3];
+// Champ[] champArray = new Champ[3];
 
-champArray[0] = new 애쉬();
-champArray[1] = new 케이틀린();
-champArray[2] = new 야스오();
+// champArray[0] = new 애쉬();
+// champArray[1] = new 케이틀린();
+// champArray[2] = new 야스오();
 
-for (int i = 0; i < champArray.length; i++) {
+// for (int i = 0; i < champArray.length; i++) {
     
-    champArray[i].skill.plusPoint();
-    
-
-    // 항상 같은 메소드를 사용하기 때문에
-    // 메소드 이름에 대 한 고민을 할 필요가 없어서
-    // 신뢰성이 높아진다
-    System.out.println(champArray[i].skill.getDamage());
+//     champArray[i].skill.plusPoint();
     
 
-    }
+//     // 항상 같은 메소드를 사용하기 때문에
+//     // 메소드 이름에 대 한 고민을 할 필요가 없어서
+//     // 신뢰성이 높아진다
+//     System.out.println(champArray[i].skill.getDamage());
+    
 
-    }
-}
+//     }
+
+//     }
+// }
